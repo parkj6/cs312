9e66a33525b48b4d9b36bc4e38d27c254de14468
CentOS User <centosuser@localhost.localdomain>
2019-05-20 20:39:22 -0700

    everything done except instructions

--- HW7/hw7script.sh
+++ HW7/hw7script.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 
-echo -e "\e[1;32mGenerating ssh key to current location\e[0m"
-ssh-keygen -t rsa 
-#disable the line above to stop generating new keys everytime.
-
-echo -e "\e[1;36mNote that this will generate new key everytime unless you comment out the previous command (line:4) in the script" 
-sleep 5
+if [ -e ~/.ssh/id_rsa.pub ]
+then
+	echo -e "\e[1;36mSkipping ssh key generation (file exists).\e[0m"
+	echo
+	echo
+	sleep 3
+else
+	echo -e "\e[1;32mGenerating ssh key...\e[0m"
+	ssh-keygen -t rsa 
+	$not_my_first_time=1
+fi
 
 echo -e "\e[1;32mCopying the ssh key to Alpine VM's\e[0m"
 ssh-copy-id root@192.168.1.21
@@ -16,7 +21,7 @@ ssh-copy-id root@192.168.1.24
 echo -e "\e[1;32mConfiguring Ansible Playbook\e[0m"
 ansible-playbook webserver.yaml -i hosts.ini
 
-echo -e "\e[1;32mDo you even curl bro?\e[0m"
+echo -e "\e[1;32mDO YOU EVEN CURL, BRO?\e[0m"
 curl 192.168.1.21
 sleep 2
 curl 192.168.1.22
